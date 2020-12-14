05d22ca177c417f1f2e22e2c5d5d08e6295b5c68
Tejas Khare <49532169+tejaskhare99@users.noreply.github.com>
2020-12-14 17:30:10 +0530

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,16 @@
+from network import net
+from LoadData import testloader
+import os
+import torch 
+import torchvision
+import torch.nn as nn
+
+def test_image_reconstruction(net, testloader):
+     for batch in testloader:
+        img, _ = batch
+        img = img.to(device)
+        img = img.view(img.size(0), -1)
+        outputs = net(img)
+        outputs = outputs.view(outputs.size(0), 1, 28, 28).cpu().data
+        save_image(outputs, 'fashionmnist_reconstruction.png')
+        break
