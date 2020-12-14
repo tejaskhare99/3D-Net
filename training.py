aa110e5d2f9b5ec9be2c721685f2e1fd11eddc90
Tejas Khare <49532169+tejaskhare99@users.noreply.github.com>
2020-12-14 17:29:05 +0530

    Update training.py

---
+++ training.py
@@ -0,0 +1,31 @@
+from network import net
+from LoadData import trainloader
+import os
+import torch 
+import torchvision
+import torch.nn as nn
+
+def train(net, trainloader, NUM_EPOCHS):
+    train_loss = []
+    for epoch in range(NUM_EPOCHS):
+        running_loss = 0.0
+        for data in trainloader:
+            img, _ = data
+            img = img.to(device)
+            img = img.view(img.size(0), -1)
+            # print(img.shape)
+            
+            optimizer.zero_grad()
+            outputs = net(img)
+            loss = criterion(outputs, img)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        
+        loss = running_loss / len(trainloader)
+        train_loss.append(loss)
+        print('Epoch {} of {}, Train Loss: {:.3f}'.format(
+            epoch+1, NUM_EPOCHS, loss))
+        if epoch % 5 == 0:
+            save_decoded_image(outputs.cpu().data, epoch)
+    return train_loss
