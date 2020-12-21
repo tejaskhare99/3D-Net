0c18222fbce23c75811923e4038970d2c4b5354b
Parag Chaudhari <pc0c88411@techmahindra.com>
2020-12-21 11:44:19 +0530

    added plot function

--- training.py
+++ training.py
@@ -9,6 +9,7 @@ def train(net, trainloader, optimizer, criterion, device, NUM_EPOCHS):
         with tqdm(trainloader, unit='batch') as tepoch:
             running_loss = 0.0
             for (image, image_3d) in tepoch:
+
                 img = image.to(device).float()
 
                 image_3d = image_3d.to(device)
@@ -21,7 +22,8 @@ def train(net, trainloader, optimizer, criterion, device, NUM_EPOCHS):
                 loss.backward()
                 optimizer.step()
                 running_loss += loss.item()
-                plot3d(outputs[0].cpu().numpy())
+                plot3d(outputs[0].detatch().numpy())
+
 
         loss = running_loss / len(trainloader)
         train_loss.append(loss)
